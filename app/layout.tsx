230b78de52bf3dbd550eaf88adb5e76f48bf33c9
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-04-14 18:33:03 +0530

    add shadcn sidebar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 
 import './globals.css'
 import { ThemeProvider } from '@/providers/theme-provider'
+import { TooltipProvider } from '@/components/ui/tooltip'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -37,7 +38,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <TooltipProvider>{children}</TooltipProvider>
         </ThemeProvider>
       </body>
     </html>
