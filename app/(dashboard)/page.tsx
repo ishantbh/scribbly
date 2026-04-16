6e95b8a5e6db178c92207d5fd8ff2c1adc553b04
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-04-16 15:09:52 +0530

    add user button to test logout and complete auth flow

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,9 @@
+import { UserButton } from '@clerk/nextjs'
+
 export default function Home() {
   return (
     <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
+      <UserButton />
       <div className='grid auto-rows-min gap-4 md:grid-cols-3'>
         {Array.from({ length: 3 }).map((_, i) => (
           <div key={i} className='aspect-video rounded-xl bg-muted/50' />
